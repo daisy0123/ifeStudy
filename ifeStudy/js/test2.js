5a85d0c7b473e2d0e471c09742b907e9f9a3421f
qiuyuxiu <1264575091@qq.com>
2017-02-24 15:29:47 +0800

    ifeStudy

---
+++ ifeStudy/js/test2.js
@@ -0,0 +1,11 @@
+var allMeau = document.getElementById("allMeau");
+var meau = document.getElementById("meau");
+
+function myFunction() {
+	if(meau.style.display =='none'){
+		meau.style.display  = 'block';
+	}else if(meau.style.display =='block'){
+		meau.style.display  = 'none';
+	}
+	
+}
